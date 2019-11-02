39f5fbf015d50433d4fd91544b8d4a8b209a4005
Rinko <1957880233@qq.com>
2019-11-02 12:23:22 +0800

    第七次作业

---
+++ NewHomework/Homework_7/Default.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+// 一个用以描述几何形状的class
+class Shape{
+public:
+    enum ShapeColor { Red, Green, Blue };
+
+    // 所有形状都必须提供一个函数，用来绘制出自己
+    virtual void draw(ShapeColor color = Red) const = 0;
+};
+
+class Rectangle: public Shape{
+public:
+    // 注意，赋予不同的缺省参数值，这真糟糕
+    virtual void draw(ShapeColor color = Green) const{
+        cout << color << endl;
+    }
+};
+
+int main(){
+    Shape* r = new Rectangle();
+    r->draw();
+    cout << "你好!" << endl;
+}
---
+++ NewHomework/Homework_7/try.py
@@ -0,0 +1,5 @@
+print("hola")
+print("heihei")
+
+for i in range(5):
+    print(i)
